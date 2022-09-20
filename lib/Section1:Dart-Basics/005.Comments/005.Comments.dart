e6a254bd8b0e88560a29f37628fef01d5cc2eb8d
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2022-09-20 04:06:47 +0200

    add comment

---
+++ lib/Section1:Dart-Basics/005.Comments/005.Comments.dart
@@ -0,0 +1,16 @@
+main(){
+  // if we need to add a single line comment
+
+  // we put multi pack slash before content
+
+  // if we need to add multi-line comment we put /** **/ and put the content between it
+
+  /**
+  * hello
+  * @Author Abdelrahman
+  * this is a long comment
+  * we can add a multi lines
+  * */
+
+
+}
