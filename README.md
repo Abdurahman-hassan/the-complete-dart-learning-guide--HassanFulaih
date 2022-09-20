82c9bc889115d850e09178b4ae690c03ada6380d
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2022-09-20 02:24:10 +0200

    This is the first repo
    This is the first repo contains the project folders,

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# The Complete Dart Learning Guide - HassanFulaih
+# The Complete Dart Learning Guide
+
+In this repo, we will begin learning Dart step by step we'll go through with:
+- Dart Basics,
+- Functions, 
+- Object oriented programming,
+- asynchronous programming
---
+++ lib/Section1:Dart-Basics/001.output.dart
@@ -0,0 +1,15 @@
+main(){
+  // you can print with double " "
+  print("Hi");
+  // you can print with single ' '
+  print('Hi');
+  // you can print multiple line with triple '''  '''
+  print('''Hi,
+  my 
+  name
+   is "Abdelrahman Hassan"''');
+  // print number
+  print(5+3);
+  // print word
+  print("5+3");
+}
