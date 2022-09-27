f4f059cbbf347873cdaaca99db8f219126dc3e18
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2022-09-27 04:11:17 +0200

    Edit on input file and upload the exception folder
    In this commit I Edit on 015.input by removing the condition from the file and upload the exception folder

--- lib/Section1:Dart-Basics/015.Input/015.Input.dart
+++ lib/Section1:Dart-Basics/015.Input/015.Input.dart
@@ -1,16 +1,15 @@
 import 'dart:io';
 
 void main() {
+  // without exception
+
   print(
       "Please enter your birthYear to get your age or you can enter your age to get your birthYear");
-  var birthYear = num.parse(stdin.readLineSync()!);
-  if (birthYear is int) {
+    var birthYear = num.parse(stdin.readLineSync()!);
     var age = DateTime.now().year - birthYear;
     if (birthYear.clamp(1820, DateTime.now().year) == birthYear)
       print("Your Age is $age");
     else
       print("Your birthYear is $age");
-  } else {
-    print("please enter a valid integer number");
-  }
+
 }
