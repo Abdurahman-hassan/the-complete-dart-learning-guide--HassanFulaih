9ee60f0e3e9ae93605135e1d738e778227f2d688
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2022-09-27 04:47:20 +0200

    Add the List folder

---
+++ lib/Section1:Dart-Basics/019.Lists/019.Lists.dart
@@ -0,0 +1,51 @@
+main() {
+  var list1 = [4, 5, 6, 7, 8, 9];
+  print(list1);
+  print(list1[0]);
+  print(list1[3]);
+  print(list1.first);
+  print(list1.last);
+
+  print(list1.reversed);
+
+  list1[1] = 1;
+  print(list1);
+
+  list1.add(10);
+  print(list1);
+  // add 10 after the last index
+  //[4, 1, 6, 7, 8, 9, 10]
+
+  list1.addAll([3, 7, 8]);
+  print(list1);
+  // add [3, 7, 8] after the last index
+
+
+  // insert the 8 in the first index
+  list1.insert(0, 8);
+  print(list1);
+
+  // insert the [3, 7, 8] in the first index
+  list1.insertAll(0, [3, 7, 8]);
+  print(list1);
+
+  list1.remove(9);
+  // remove the value 9 not index 9
+  print(list1);
+
+  list1.removeAt(9);
+  // remove the index 9 not value 9
+  print(list1);
+
+  list1.removeLast();
+  // remove the last value
+  print(list1);
+
+  list1.removeRange(0, 5);
+  // remove the range indexes from 0 to 5
+  print(list1);
+
+  list1.replaceRange(0, 5, [1,2,3,4,5]);
+  print(list1);
+
+}
