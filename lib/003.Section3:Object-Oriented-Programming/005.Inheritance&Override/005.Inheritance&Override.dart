bce8ca894f7898b47d51644f663f779b1c937c23
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2022-09-29 08:38:07 +0200

    Add the Inheritance&Override folder

---
+++ lib/003.Section3:Object-Oriented-Programming/005.Inheritance&Override/005.Inheritance&Override.dart
@@ -0,0 +1,41 @@
+main() {
+  A a = A();
+  a.info();
+
+  B b = B();
+  b.info();
+
+  A b2 = B();
+  b2.info();
+
+  C c = C();
+  c.info();
+
+  A c2 = C();
+  c2.info();
+
+  B c3 = C();
+  c3.info();
+}
+
+class A {
+  info() {
+    print('Hello A');
+  }
+}
+
+class B extends A {
+  @override
+  info() {
+    print("hello B");
+    // return super.info();
+  }
+}
+
+class C extends B {
+  @override
+  info() {
+    print("hello C");
+    // return super.info();
+  }
+}
