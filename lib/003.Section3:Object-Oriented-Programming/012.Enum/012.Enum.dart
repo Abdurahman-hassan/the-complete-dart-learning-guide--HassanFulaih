ecfb6c2839ad50328d5adea9a1f6f61078af6bd7
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2022-09-29 23:01:10 +0200

    Add the Enum folder

---
+++ lib/003.Section3:Object-Oriented-Programming/012.Enum/012.Enum.dart
@@ -0,0 +1,41 @@
+main() {
+  //
+  print(printDataWithoutTheEnumName(Months.Aug));
+}
+
+enum Months { Jen, Feb, Mar, Jun, Jul, Aug, Sep, Oct, Nov, Dec }
+
+printDataWithoutTheEnumName(Months m) {
+  switch (m) {
+    case Months.Jen:
+      return "Jen";
+      break;
+    case Months.Feb:
+      return "Feb";
+      break;
+    case Months.Mar:
+      return "Mar";
+      break;
+    case Months.Jun:
+      return "Jun";
+      break;
+    case Months.Jul:
+      return "Jul";
+      break;
+    case Months.Aug:
+      return "Aug";
+      break;
+    case Months.Sep:
+      return "Sep";
+      break;
+    case Months.Oct:
+      return "Oct";
+      break;
+    case Months.Nov:
+      return "Nov";
+      break;
+    case Months.Dec:
+      return "Dec";
+      break;
+  }
+}
