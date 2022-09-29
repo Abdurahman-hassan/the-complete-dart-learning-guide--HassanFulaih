99e49afa0c43329cfe340e3dee0ce03af5e51791
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2022-09-29 06:40:50 +0200

    Add the Static&This folder

---
+++ lib/003.Section3:Object-Oriented-Programming/003.Static&This/003.Static&This.dart
@@ -0,0 +1,79 @@
+class Human {
+  static String? staticFirstName;
+  static String? staticLastName;
+  static int? staticAge;
+  static double? staticLength;
+
+  String? firstName;
+  String? lastName;
+  int? age;
+  double? length;
+
+  Human({firstNameCon, lastNameCon, ageCon, lengthCon}) {
+    firstName = firstNameCon;
+    lastName = lastNameCon;
+    age = ageCon;
+    length = lengthCon;
+  }
+
+  static void info({firstName, lastName, hAge, hLength = 180.0}) {
+    staticFirstName = firstName;
+    staticLastName = lastName;
+    staticAge = hAge;
+    staticLength = hLength;
+
+    print("First name : $staticFirstName");
+    print("Lastname name : $staticLastName");
+    print("Age : $staticAge ");
+    print("Length : $staticLength");
+  }
+
+  void info2({firstName, lastName, age, length = 180.0}) {
+    // we can't use this inside static function
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.length = length;
+
+    print("First name : $firstName");
+    print("Lastname name : $lastName");
+    print("Age : $age ");
+    print("Length : $length");
+  }
+
+  void info3({firstName, lastName, age, Length = 180.0}) {
+    print("First name : $staticFirstName");
+    print("Lastname name : $lastName");
+    print("Age : $age ");
+    print("Length : $Length");
+  }
+}
+
+main() {
+  // call the fun through the Class name directly
+  Human.info(
+      firstName: Human.staticFirstName = "A", lastName: "Hassan", hAge: 25);
+  print('');
+
+  Human humanObject = Human();
+  humanObject.info2(
+      firstName: "Gamal", lastName: "Ayman", age: 26, length: 182.0);
+  // the first name will printed as null if we didn't call the info2 method
+  // because it has an attribute variable this.lastName = lastName;
+  print("the method attribute that initialized through function is ${humanObject.firstName}");
+
+  print('');
+
+  Human humanObject2 = Human(lastNameCon: "GamalEldeen");
+  Human.staticFirstName = "Gamal-Static";
+  humanObject2.info3(lastName: "Ayman", age: 27, Length: 182.0);
+  // the last name will printed as null if we didn't assign a value in a param constructor
+  print("the method attribute in humanObject2 that initialized through constructor is ${humanObject2.lastName}");
+
+  Human humanObject3 = Human(
+      firstNameCon: "Abdelrahman",
+      lastNameCon: "Hassan",
+      ageCon: 20,
+      lengthCon: 188.0);
+  print("the method attribute in humanObject3 that initialized through constructor is ${humanObject3.lastName}");
+}
