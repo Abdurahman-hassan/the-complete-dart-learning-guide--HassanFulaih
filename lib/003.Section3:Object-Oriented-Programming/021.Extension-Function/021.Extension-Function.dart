42d73667958cac39dc33741a96fe8541c75fcd6f
abdurahman-hassan <abdelrahmanhassan@paymob.com>
2022-09-30 07:06:31 +0200

    Upload Extension-Function folder

---
+++ lib/003.Section3:Object-Oriented-Programming/021.Extension-Function/021.Extension-Function.dart
@@ -0,0 +1,16 @@
+void main() {
+  print(int.parse(' 2 ') + 5);
+  // or
+  print(NumberParsing('2').parseInt() + 5);
+
+  print('2'.parseInt());
+}
+
+extension NumberParsing on String {
+  // this function has been added to the class functions
+
+  parseInt() {
+    // this is a '2' the object that call the extention function
+    return int.parse(this);
+  }
+}
